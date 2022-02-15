68ec57afa5d40568587279a9220581171854cb0c
Matti Luukkainen <mluukkai@iki.fi>
2022-02-15 11:32:40 +0200

    part8-6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { useState } from 'react'
-import { useQuery } from '@apollo/client'
+import { useQuery, useApolloClient } from '@apollo/client'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 
 import { ALL_PERSONS } from './queries'
 
@@ -17,6 +18,14 @@ const Notify = ({ errorMessage }) => {
 const App = () => {
   const result = useQuery(ALL_PERSONS)
   const [errorMessage, setErrorMessage] = useState(null)
+  const [token, setToken] = useState(null)
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
 
   if (result.loading) {
     return <div>loading...</div>
@@ -29,9 +38,19 @@ const App = () => {
     }, 10000)
   }
 
+  if (!token) {
+    return (
+      <>
+        <Notify errorMessage={errorMessage} />
+        <LoginForm setToken={setToken} setError={notify} />
+      </>
+    )
+  }
+
   return (
     <>
       <Notify errorMessage={errorMessage} />
+      <button onClick={logout}>logout</button>
       <Persons persons={result.data.allPersons} />
       <PersonForm setError={notify} />
       <PhoneForm setError={notify} />
