bdc190c18c343fb6b65f52f9db82e189da5caf83
Matti Luukkainen <mluukkai@iki.fi>
2023-02-02 20:47:29 +0200

    part8-1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { gql, useQuery } from '@apollo/client'
 
-function App() {
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons  {
+      name
+      phone
+      id
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Persons persons={result.data.allPersons} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
