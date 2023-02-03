ec841384f172eb983f0b2b2322e850ea6fc4737d
Matti Luukkainen <mluukkai@iki.fi>
2023-02-03 23:05:48 +0200

    part8-7

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -10,12 +10,18 @@ const PersonForm = ({ setError }) => {
   const [city, setCity] = useState('')
 
   const [ createPerson ] = useMutation(CREATE_PERSON, {
-    refetchQueries: [ { query: ALL_PERSONS } ],
     onError: (error) => {
       const errors = error.graphQLErrors[0].extensions.error.errors
       const messages = Object.values(errors).map(e => e.message).join('\n')
       setError(messages)
-    }
+    },
+    update: (cache, response) => {
+      cache.updateQuery({ query: ALL_PERSONS }, ({ allPersons }) => {
+        return {
+          allPersons: allPersons.concat(response.data.addPerson),
+        }
+      })
+    },
   })
 
   const submit = async (event) => {
